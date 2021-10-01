573dd9285f635ca68570ab8714e1802ffe718f14
Kyle Wigley <kyle@dbtlabs.com>
2021-10-01 14:03:11 -0400

    --wip--

--- dbt_server/services/dbt_service.py
+++ dbt_server/services/dbt_service.py
@@ -42,17 +42,8 @@ def dbt_run_sync(project_path, args, manifest):
 
 
 def dbt_list(project_path, args, manifest):
-    config = _get_dbt_config(project_path)
-    def no_op(*args, **kwargs):
-        pass
-
-    # Create the task
-    task = ListTask(args, config)
-    # Wow! We can monkeypatch taskCls.load_manifest to return _our_ manifest
-    # TODO : Let's update Core to support this kind of thing more natively?
-    task.load_manifest = no_op
-    task.manifest = manifest
-
+    config = get_dbt_config(project_path)
+    task = create_task('list', args, manifest, config)
     return task.run()
 
 
