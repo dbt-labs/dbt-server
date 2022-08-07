1540984005cfd670d12980b7a1deabba274d816b
David Harting <david.e.harting@gmail.com>
2022-08-07 15:48:11 -0400

    document tmp_dir context

--- tests/context.py
+++ tests/context.py
@@ -7,6 +7,12 @@ from fs.tempfs import TempFS
 
 @contextmanager
 def temp_dir(identifier: str = "test") -> Generator[str, None, None]:
+    """
+    Creates a temporary directory and yields its path.
+    The temporary directory is destroyed when the context block is exited.
+
+    Created as an alternative to the pytest tmpdir fixture, because Python type hints are lost when injecting pytest fixtures into tests.
+    """
     id = uuid.uuid4()
     fs = TempFS(identifier=f"{identifier}-{id}")
 
