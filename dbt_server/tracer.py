5e86e53b8d65e276d4437c227fb4eadf589e7524
Rachel <41338402+racheldaniel@users.noreply.github.com>
2022-08-31 08:56:57 -0500

    Adds dbt version to tracing (#82)
    * Adds dbt version to tracing
    
    * Pulls dbt version from core instead of env vars
    
    * Fixes linting errors

--- dbt_server/tracer.py
+++ dbt_server/tracer.py
@@ -1,4 +1,5 @@
 import os
+from dbt import version
 
 APM_ENABLED = os.getenv("APPLICATION_TRACING_ENABLED", "") not in (
     "",
@@ -9,6 +10,7 @@ APM_ENABLED = os.getenv("APPLICATION_TRACING_ENABLED", "") not in (
 
 ENV_HAS_DDTRACE = False
 TRACING_ENABLED = False
+DBT_VERSION = str(version.installed).lstrip("=")
 
 try:
     import ddtrace
@@ -39,7 +41,19 @@ def wrap(func):
 
     def dd_trace(*args, **kwargs):
         name = f"{func.__module__}.{func.__name__}"
-        with ddtrace.tracer.trace(name):
+        try:
+            # Should contain only one adapter
+            adapter_map = {k: v for k, v in version._get_dbt_plugins_info()}
+            adapter_name = list(adapter_map.keys())[0]
+            adapter_version = adapter_map[adapter_name]
+        # In case this private method is changed
+        except AttributeError:
+            adapter_name = ""
+            adapter_version = ""
+
+        with ddtrace.tracer.trace(name) as span:
+            span.set_tag("dbt.version", DBT_VERSION)
+            span.set_tag(f"dbt.adapters.{adapter_name}", adapter_version)
             return func(*args, **kwargs)
 
     if ENV_HAS_DDTRACE and APM_ENABLED:
