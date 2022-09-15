f65974c870816bd73d80d08b13809bcaec5d3182
Vadim Rybak <vadim82@gmail.com>
2022-09-15 15:32:02 -0400

    Fixed typo

--- dbt_server/views.py
+++ dbt_server/views.py
@@ -32,7 +32,7 @@ from sqlalchemy.orm import Session
 from sse_starlette.sse import EventSourceResponse
 from starlette.requests import Request
 
-# We need to override the EVENT_HOSTORY queue to store
+# We need to override the EVENT_HISTORY queue to store
 # only a small amount of events to prevent too much memory
 # from being used.
 dbt.events.functions.EVENT_HISTORY = deque(maxlen=10)
