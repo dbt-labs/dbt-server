7ce265a5d12c844289160195d00528d48b5e9154
Peter Bertuglia <peter.bertuglia@dbtlabs.com>
2022-09-20 08:06:11 -0700

    Support core 1.3.0b2 in dbt-server (#90)
    * Removed all code related to deps and handled both function names for get_compatible_versions
    
    * removed more unused code
    
    * removed some unused imports

--- dbt_server/services/dbt_service.py
+++ dbt_server/services/dbt_service.py
@@ -1,10 +1,5 @@
-import json
 import os
 import threading
-from collections import namedtuple
-from typing import Optional
-
-from pydantic import BaseModel
 
 from dbt_server.services import filesystem_service
 from dbt_server.exceptions import (
@@ -14,13 +9,8 @@ from dbt_server.exceptions import (
 )
 from dbt_server import tracer
 
-from dbt.clients.registry import package_version, get_available_versions
-from dbt import semver
 from dbt.exceptions import (
-    VersionsNotCompatibleException,
     ValidationException,
-    DependencyException,
-    package_version_not_found,
     CompilationException,
 )
 from dbt.lib import (
@@ -112,55 +102,6 @@ def deserialize_manifest(serialize_path):
     return dbt_deserialize_manifest(manifest_packed)
 
 
-def dbt_deps(project_path):
-    # TODO: add this to dbt lib! this is not great
-    from dbt.task.deps import DepsTask
-    from dbt.config.runtime import UnsetProfileConfig
-    from dbt import flags
-    import dbt.adapters.factory
-    import dbt.events.functions
-
-    disable_tracking()
-
-    class Args(BaseModel):
-        profile: Optional[str] = None
-        target: Optional[str] = None
-        single_threaded: Optional[bool] = None
-        threads: Optional[int] = None
-
-    if os.getenv("DBT_PROFILES_DIR"):
-        profiles_dir = os.getenv("DBT_PROFILES_DIR")
-    else:
-        profiles_dir = os.path.expanduser("~/.dbt")
-
-    RuntimeArgs = namedtuple(
-        "RuntimeArgs", "project_dir profiles_dir single_threaded which"
-    )
-
-    # Construct a phony config
-    try:
-        config = UnsetProfileConfig.from_args(
-            RuntimeArgs(project_path, profiles_dir, True, "deps")
-        )
-    except ValidationException:
-        raise InvalidConfigurationException(
-            "Invalid dbt config provided. Check that your credentials are configured"
-            " correctly and a valid dbt project is present"
-        )
-    # Clear previously registered adapters--
-    # this fixes cacheing behavior on the dbt-server
-    flags.set_from_args("", config)
-    dbt.adapters.factory.reset_adapters()
-    # Set invocation id
-    dbt.events.functions.set_invocation_id()
-    task = DepsTask(Args(), config)
-
-    # TODO: 🤦 reach into dbt-core
-    task.config.packages_install_path = os.path.join(project_path, "dbt_packages")
-
-    return task.run()
-
-
 def dbt_run(project_path, args, manifest):
     config = create_dbt_config(project_path, args)
     task = create_task("run", args, manifest, config)
@@ -241,61 +182,3 @@ def compile_sql(manifest, project_path, sql):
         )
 
     return result.to_dict()
-
-
-def render_package_data(packages):
-    return json.loads(packages)
-
-
-def get_package_details(package_data):
-    packages = []
-    for package in package_data.get("packages", {}):
-        full_name = package.get("package")
-        version = resolve_version(package)
-        if not full_name or not version:
-            # TODO: Something better than this for detecting Hub packages?
-            logger.debug(
-                f"Skipping package: {package}. "
-                "Either non-hub package or missing package version."
-            )
-            continue
-        tar_name = "{}.{}.tar.gz".format(full_name, version)
-        package_details = package_version(full_name, version)
-        tarball = package_details.get("downloads", {}).get("tarball")
-        name = package_details.get("name")
-        packages.append(
-            {
-                # Hack to imitate core package name
-                "package": f"{full_name}@{version}",
-                "name": name,
-                "version": version,
-                "tar_name": tar_name,
-                "tarball": tarball,
-            }
-        )
-    return packages
-
-
-def resolve_version(package) -> str:
-    versions = package.get("version")
-    install_prerelease = package.get("install-prerelease")
-    if not isinstance(versions, list):
-        return versions
-    try:
-        range_ = semver.reduce_versions(*versions)
-    except VersionsNotCompatibleException as e:
-        new_msg = "Version error for package {}: {}".format(package.get("package"), e)
-        raise DependencyException(new_msg) from e
-
-    available = get_available_versions(package.get("package"))
-    prerelease_version_specified = any(
-        bool(semver.VersionSpecifier.from_version_string(version).prerelease)
-        for version in versions
-    )
-    installable = semver.filter_installable(
-        available, install_prerelease or prerelease_version_specified
-    )
-    target = semver.resolve_to_specific_version(range_, installable)
-    if not target:
-        package_version_not_found(package, range_, installable)
-    return target
