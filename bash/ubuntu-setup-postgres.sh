8c04638d3df23ec10e4c38e241e58d8ef9b92fbd
dichenqiandbt <dichen.qian@dbtlabs.com>
2023-03-01 12:09:34 -0800

    Enable smoke test.

---
+++ bash/ubuntu-setup-postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Bash scripts to install postgresql server in ubuntu image.
+# It also creates "root" user with all priviledges on "postgres" database for 
+# testing purpose. The password is "testpassword".
+apt update
+apt install postgresql postgresql-contrib
+service postgresql start
+su postgres
+psql -c '\x' -c "CREATE USER root WITH PASSWORD 'testpassword';"
+psql -c '\x' -c "GRANT ALL PRIVILEGES ON DATABASE postgres TO root;"
+exit
