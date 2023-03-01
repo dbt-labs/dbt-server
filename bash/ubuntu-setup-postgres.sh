8addd10a6d4325949683e21a9a267397c213eb21
dichenqiandbt <dichen.qian@dbtlabs.com>
2023-03-01 14:12:55 -0800

    Fix sudo

--- bash/ubuntu-setup-postgres.sh
+++ bash/ubuntu-setup-postgres.sh
@@ -2,13 +2,13 @@
 # Bash scripts to install postgresql server in ubuntu image.
 # It also creates "root" user with all priviledges on "postgres" database for 
 # testing purpose. The password is "testpassword".
-apt update
-apt install postgresql postgresql-contrib -y
-service postgresql start
+sudo apt update
+sudo apt install postgresql postgresql-contrib -y
+sudo service postgresql start
 TEMP_SQL_FILE=/tmp/__temp.sql
 cat <<EOF  >${TEMP_SQL_FILE}
 \x
 CREATE USER root WITH PASSWORD 'testpassword';
 GRANT ALL PRIVILEGES ON DATABASE postgres TO root;
 EOF
-su postgres -c "psql -f ${TEMP_SQL_FILE}"
+sudo su postgres -c "psql -f ${TEMP_SQL_FILE}"
