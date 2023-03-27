83a8250af71f231f48818d110c8beff944c316f4
dichenqiandbt <dichen.qian@dbtlabs.com>
2023-03-27 16:43:57 -0700

    nits

--- bash/start-celery.sh
+++ bash/start-celery.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# TODO: Detach mode used here can't be easily stopped or restarted, we may 
+# consider use init.d for better maintenance.
 celery -A dbt_worker.app worker --loglevel=INFO --detach
