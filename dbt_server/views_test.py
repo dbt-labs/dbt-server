c4e8bfbc3ea476fe97d06589d5fbbe34c2f5412a
Rachel Daniel <rachel.daniel@fishtownanalytics.com>
2023-03-31 13:05:56 -0500

    Pulls project path from env var and fixes some tests

--- dbt_server/views_test.py
+++ dbt_server/views_test.py
@@ -63,7 +63,7 @@ class TestPostInvocation(IsolatedAsyncioTestCase):
             )
         )
         mock_invoke.apply_async.assert_called_once_with(
-            args=[TEST_COMMAND_WITH_PROJECT_DIR, TEST_URL], task_id=TEST_TASK_ID
+            args=[TEST_COMMAND_WITH_PROJECT_DIR, TEST_DIR, TEST_URL], task_id=TEST_TASK_ID
         )
         mock_invoke.backend.store_result.assert_called_once_with(
             TEST_TASK_ID, None, "PENDING"
@@ -88,7 +88,7 @@ class TestPostInvocation(IsolatedAsyncioTestCase):
             )
         )
         mock_invoke.apply_async.assert_called_once_with(
-            args=[TEST_COMMAND_WITH_PROJECT_DIR, TEST_URL], task_id=TEST_TASK_ID
+            args=[TEST_COMMAND_WITH_PROJECT_DIR, None, TEST_URL], task_id=TEST_TASK_ID
         )
         mock_invoke.backend.store_result.assert_called_once_with(
             TEST_TASK_ID, None, "PENDING"
@@ -111,7 +111,7 @@ class TestPostInvocation(IsolatedAsyncioTestCase):
             PostInvocationRequest(command=TEST_COMMAND, callback_url=TEST_URL)
         )
         mock_invoke.apply_async.assert_called_once_with(
-            args=[TEST_COMMAND_WITH_PROJECT_DIR, TEST_URL], task_id=TEST_TASK_ID
+            args=[TEST_COMMAND_WITH_PROJECT_DIR, TEST_DIR, TEST_URL], task_id=TEST_TASK_ID
         )
         mock_invoke.backend.store_result.assert_called_once_with(
             TEST_TASK_ID, None, "PENDING"
@@ -134,7 +134,7 @@ class TestPostInvocation(IsolatedAsyncioTestCase):
             )
         )
         mock_invoke.apply_async.assert_called_once_with(
-            args=[TEST_COMMAND, TEST_URL], task_id="USER_TASK_ID"
+            args=[TEST_COMMAND, None, TEST_URL], task_id="USER_TASK_ID"
         )
         mock_invoke.backend.store_result.assert_called_once_with(
             "USER_TASK_ID", None, "PENDING"
